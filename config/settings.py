87863f5ca91861fc1088501b6464d3a32ea2644c
ivanvallejoss <ivanvallejos06@gmail.com>
2026-07-24 09:54:51 -0300

    refactor(config): refactored the configuration to captured the traceback error with the journal system tool

--- config/settings.py
+++ config/settings.py
@@ -82,3 +82,20 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {"class": "logging.StreamHandler"},
+    },
+    "root": {"handlers": ["console"], "level": "WARNING"},
+    "loggers": {
+        "django.request": {
+            "handlers": ["console"],
+            "level": "ERROR",
+            "propagate": False,
+        },
+    },
+}
